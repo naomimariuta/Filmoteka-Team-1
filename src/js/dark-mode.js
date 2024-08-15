10b0bf005ee774ff4cf20ebaf54bbf23aeb768a8
naomimariuta <157979968+naomimariuta@users.noreply.github.com>
2024-08-16 02:25:14 +0300

    last commit

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,10 +1,15 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
+const watchedBtn = document.querySelector('.watched-btn');
+const queueBtn = document.querySelector('.queue-btn');
+const actionPage = document.querySelector('.active_link');
 
 themeToggleBtn.addEventListener('click', () => {
   document.querySelector('.home-btn').classList.toggle('dark-mode');
+  if (actionPage.dataset.action === 'library') {
+    watchedBtn.classList.toggle('dark-mode');
+    queueBtn.classList.toggle('dark-mode');
+  }
   document.querySelector('.library-btn').classList.toggle('dark-mode');
-  document.querySelector('.watched-btn').classList.toggle('dark-mode');
-  document.querySelector('.queue-btn').classList.toggle('dark-mode');
   const titles = document.querySelectorAll('.card-list-title');
   titles.forEach(title => {
     title.classList.toggle('dark-mode');
